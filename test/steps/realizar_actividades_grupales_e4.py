a08777164f067d975b6d3e331c528b56d94937a3
Jean Cotera <jean.cotera@gmail.com>
2025-01-07 20:26:10 -0500

    Solucion de errores

--- test/steps/realizar_actividades_grupales_e4.py
+++ test/steps/realizar_actividades_grupales_e4.py
@@ -1,20 +1,5 @@
 from behave import *
 
-@step('que el ciudadano tiene una reserva en el "Parque la Alameda"')
-def step_impl(context, espacio_publico, fecha, hora_inicio, hora_fin):
-    context.agenda = Agenda()
-    context.reserva = Reserva(espacio_publico, fecha, hora_inicio, hora_fin)
-    context.agenda.reservar_espacio_publico(context.reserva)
-    assert context.agenda.esta_reserva_en_agenda(context.reserva)
-
-
-
-@step("cancele la reserva")
-def step_impl(context):
-    assert context.agenda.cancelar_reserva(context.reserva)
-
-
-
 @step("se enviará una correo de cancelacion de cancelación a los invitados.")
 def step_impl(context):
     assert not context.agenda.esta_reserva_en_agenda(context.reserva)
