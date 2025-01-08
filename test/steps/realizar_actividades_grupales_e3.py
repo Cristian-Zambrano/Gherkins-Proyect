ecc2cdf84c8bd3d8b9bbe5baf927d8c07383de7e
Jorman Chuquer <jorman.chuquer@epn.edu.ec>
2025-01-07 19:01:52 -0500

    steps e3

--- test/steps/realizar_actividades_grupales_e3.py
+++ test/steps/realizar_actividades_grupales_e3.py
@@ -1,19 +1,23 @@
 from behave import *
 
-use_step_matcher("re")
+
 
 
 @step('que el ciudadano tiene una reserva en el "Parque la Alameda"')
+def step_impl(context, espacio_publico, fecha, hora_inicio, hora_fin):
+    context.agenda = Agenda()
+    context.reserva = Reserva(espacio_publico, fecha, hora_inicio, hora_fin)
+    context.agenda.reservar_espacio_publico(context.reserva)
+    assert context.agenda.esta_reserva_en_agenda(context.reserva)
+
+
+
+@step("cancele la reserva")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Dado que el ciudadano tiene una reserva en el "Parque la Alameda"')
+    assert context.agenda.cancelar_reserva(context.reserva)
+
 
 
 @step("la reserva será eliminada de la agenda pública\.")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Entonces la reserva será eliminada de la agenda pública.')
+    assert not context.agenda.esta_reserva_en_agenda(context.reserva)
