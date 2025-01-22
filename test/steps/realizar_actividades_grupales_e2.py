2c003ee114a1990058c3ae1d2a0096004cde843e
Cristian Zambrano <94475992+Cristian-Zambrano@users.noreply.github.com>
2025-01-22 12:11:49 -0500

    Update realizar_actividades_grupales_e2.py

--- test/steps/realizar_actividades_grupales_e2.py
+++ test/steps/realizar_actividades_grupales_e2.py
@@ -15,14 +15,15 @@ def step_impl(context, espacio_publico, fecha_reserva, hora_inicio, hora_fin):
     context.agenda.registrar_reserva(context.reserva)
     assert (context.agenda.esta_reserva_en_agenda(context.reserva))
 
-@step("se enviará una invitación por correo con los detalles de la reserva.")
-def step_impl(context):
-    assert context.reserva.enviar_invitacion()
-
-
 @step('agregue los correos de los invitados "jean.cotera@epn.edu, cristian.sangucho@epn.edu.ec" a la reserva')
 def step_impl(context):
     invitados = ["jean.cotera@epn.edu", "cristian.sangucho@epn.edu.ec"]
     context.invitados = invitados
     context.reserva.agregar_invitados_a_reserva(context.invitados)
     assert context.reserva.existen_invitados(context.invitados)
+
+@step("se enviará una invitación por correo con los detalles de la reserva.")
+def step_impl(context):
+    assert context.reserva.enviar_invitacion()
+
+
